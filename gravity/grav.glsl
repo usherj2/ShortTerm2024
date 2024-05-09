f3b5fa79906d74553d09e57b52385769ced3d646
OrgalorgDestroyerofWorlds <james@firefly.us>
2024-05-09 14:15:42 -0400

    body struct added! overloaded method works!

--- gravity/grav.glsl
+++ gravity/grav.glsl
@@ -14,13 +14,27 @@ vec2 orbit(float speed) {
     return vec2(cos(PI * iTime / speed), sin(PI * iTime / speed));
 }
 
+struct body {
+    float mass; 
+    vec2 pos; 
+    float radius;
+};
+void new(inout body self, float mass, vec2 pos, float radius){
+    self.mass = mass;
+    self.pos = pos;
+    self.radius = radius;
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord) {
     vec2 center = iResolution.xy;
 
-    vec3 color1 = vec3(100., 100., 100.) /255.;
+    vec3 color1 = vec3(1., 1., 1.) /255.;
     vec3 color2 = vec3(255., 255., 255.) /255.;
 
     float objects = 0.;
+
+    body sun;
+    new(sun, 100., center, 50.);
 
     float earth_dist = 300.;
     float earth_speed = .5;
@@ -31,7 +45,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord) {
     vec2 earth_pos = center + (orbit(earth_speed) * earth_dist);
     vec2 moon_pos = earth_pos + (orbit(moon_speed) * moon_dist);
 
-    objects += circle(40., center, fragCoord.xy);
+    objects += circle(sun.radius, sun.pos, fragCoord.xy);
     objects += circle(20., earth_pos, fragCoord.xy);
     objects += circle(10., moon_pos, fragCoord.xy);
 
