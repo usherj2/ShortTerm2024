1dd98dcbebc305cb01cef01bfdda0d4a3a908986
OrgalorgDestroyerofWorlds <james@firefly.us>
2024-05-19 20:53:59 -0400

    added unity project

---
+++ unity/My project/Assets/Compute_Shader_testclass.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compute_Shader_testclass : MonoBehaviour
+{
+    public ComputeShader computeShader;
+    public RenderTexture renderTexture;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        renderTexture = new RenderTexture(256,256,24);
+        renderTexture.enableRandomWrite = true;
+        renderTexture.Create();
+
+        computeShader.SetTexture(0, "Result", renderTexture);
+        computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+    }
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (renderTexture == null){
+            renderTexture = new RenderTexture(256,256,24);
+            renderTexture.enableRandomWrite = true;
+            renderTexture.Create();
+        }
+        computeShader.SetTexture(0, "Result", renderTexture);
+        computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+        // Read pixels from the source RenderTexture, apply the material, copy the updated results to the destination RenderTexture
+        Graphics.Blit(renderTexture, dest);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
